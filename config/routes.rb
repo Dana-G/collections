3c016ab1affedaa7be22ee7a8d5aab53624a5e80
Brian E McElaney <brian.e.mcelaney@gmail.com>
2015-05-18 18:55:13 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # root 'welcome#index'
+
+  # Example resource route (maps HTTP verbs to controller actions
+  # automatically):
+  #   resources :products
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+end

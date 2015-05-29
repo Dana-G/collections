c60a7ec0e38ad10b57e944fd6689ca5b0b55754c
Dana Giordano <dgiordano@weblinc.com>
2015-05-29 16:37:07 -0400

    added model specs, and corrections from last commit. fixed issue 1 fixed issue 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,12 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true, format: {
-    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: {
+              with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+            }
 
   validates :password, length: { minimum: 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:create]
   get 'signup',  to: 'users#new', as: 'signup'
 
   root 'static#home'

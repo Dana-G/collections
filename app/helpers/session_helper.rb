cfda417e7e45b74aa6ebef700e3af2d7f27f78fc
Dana Giordano <dgiordano@weblinc.com>
2015-06-22 18:07:58 -0400

    changes per code review

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,18 +1,22 @@
 #
 module SessionHelper
   def signup_link
-    link_to 'Sign Up', signup_path unless current_user
+    return link_to 'Sign Up', signup_path unless current_user
+    ''
   end
 
   def login_link
-    link_to 'Login', login_path unless current_user
+    return link_to 'Login', login_path unless current_user
+    ''
   end
 
   def logout_link
-    link_to 'Logout', logout_path if current_user
+    return link_to 'Logout', logout_path if current_user
+    ''
   end
 
   def edit_link
-    link_to 'edit', edit_user_path(current_user) if current_user
+    return link_to 'edit', edit_user_path(current_user) if current_user
+    ''
   end
 end
